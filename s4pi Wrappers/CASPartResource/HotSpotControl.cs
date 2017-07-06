8e8489362418b9abfd56e17552b513c12186f931
cmarNYC <cmarinetti@nyc.rr.com>
2017-07-06 08:51:40 -0400

    Breakout of directional slider links, clarification of view angles from SimGuruModSquad

--- s4pi Wrappers/CASPartResource/HotSpotControl.cs
+++ s4pi Wrappers/CASPartResource/HotSpotControl.cs
@@ -161,22 +161,29 @@ namespace CASPartResource
             private ViewAngle angle;
             private bool flip;
             private float[] unknown7;       //2 floats
-            private ulong[] simModifierReference;      //4 Instance IDs
+            private ulong simModifierLeft;      //Instance IDs
+            private ulong simModifierRight;
+            private ulong simModifierUp;
+            private ulong simModifierDown;
             #endregion
 
             public SliderDesc(int apiVersion, EventHandler handler) : base(apiVersion, handler) { }
             public SliderDesc(int apiVersion, EventHandler handler, Stream s) : base(apiVersion, handler) { Parse(s); }
             public SliderDesc(int apiVersion, EventHandler handler, SliderDesc basis)
-                : this(apiVersion, handler, basis.angle, basis.flip, basis.unknown7, basis.simModifierReference) { }
-            public SliderDesc(int apiVersion, EventHandler handler, ViewAngle angle, bool flip, float[] unknown7, ulong[] simModifierReference)
+                : this(apiVersion, handler, basis.angle, basis.flip, basis.unknown7, 
+                basis.simModifierLeft, basis.simModifierRight, basis.simModifierUp, basis.simModifierDown) { }
+            public SliderDesc(int apiVersion, EventHandler handler, ViewAngle angle, bool flip, float[] unknown7, 
+                ulong simModifierLeft, ulong simModifierRight, ulong simModifierUp, ulong simModifierDown)
                 : base(apiVersion, handler)
             {
                 this.angle = angle;
                 this.flip = flip;
                 this.unknown7 = new float[unknown7.Length];
                 for (int i = 0; i < this.unknown7.Length; i++) this.unknown7[i] = unknown7[i];
-                this.simModifierReference = new ulong[simModifierReference.Length];
-                for (int i = 0; i < this.simModifierReference.Length; i++) this.simModifierReference[i] = simModifierReference[i];
+                this.simModifierLeft = simModifierLeft;
+                this.simModifierRight = simModifierRight;
+                this.simModifierUp = simModifierUp;
+                this.simModifierDown = simModifierDown;
             }
 
             private void Parse(Stream s)
@@ -186,8 +193,10 @@ namespace CASPartResource
                 this.flip = r.ReadBoolean();
                 this.unknown7 = new float[2];
                 for (int i = 0; i < 2; i++) { this.unknown7[i] = r.ReadSingle(); }
-                this.simModifierReference = new ulong[4];
-                for (int i = 0; i < 4; i++) { this.simModifierReference[i] = r.ReadUInt64(); }
+                this.simModifierLeft = r.ReadUInt64();
+                this.simModifierRight = r.ReadUInt64();
+                this.simModifierUp = r.ReadUInt64();
+                this.simModifierDown = r.ReadUInt64();
             }
 
             internal void UnParse(Stream s)
@@ -195,8 +204,12 @@ namespace CASPartResource
                 BinaryWriter w = new BinaryWriter(s);
                 w.Write((byte)this.angle);
                 w.Write(this.flip);
+                if (this.unknown7 == null) this.unknown7 = new float[2];
                 for (int i = 0; i < 2; i++) { w.Write(this.unknown7[i]); }
-                for (int i = 0; i < 4; i++) { w.Write(this.simModifierReference[i]); }
+                w.Write(this.simModifierLeft);
+                w.Write(this.simModifierRight);
+                w.Write(this.simModifierUp);
+                w.Write(this.simModifierDown);
             }
 
             #region AHandlerElement
@@ -207,22 +220,30 @@ namespace CASPartResource
             #region IEquatable<Vector>
             public bool Equals(SliderDesc other)
             {
-                return this.angle == other.angle && this.flip == other.flip && this.unknown7.SequenceEqual(other.unknown7) && this.simModifierReference.SequenceEqual(other.simModifierReference);
+                return this.angle == other.angle && this.flip == other.flip && this.unknown7.SequenceEqual(other.unknown7) && 
+                    this.simModifierLeft == other.simModifierLeft && this.simModifierRight == other.simModifierRight && this.simModifierUp == other.simModifierUp && this.simModifierDown == other.simModifierDown;
             }
 
             public override bool Equals(object obj) { return obj is SliderDesc && Equals(obj as SliderDesc); }
 
-            public override int GetHashCode() { return this.angle.GetHashCode() + this.flip.GetHashCode() + this.unknown7.GetHashCode() + this.simModifierReference.GetHashCode(); }
+            public override int GetHashCode() { return this.angle.GetHashCode() + this.flip.GetHashCode() + this.unknown7.GetHashCode() + 
+                this.simModifierLeft.GetHashCode() + this.simModifierRight.GetHashCode() + this.simModifierUp.GetHashCode() + this.simModifierDown.GetHashCode(); }
             #endregion
 
             [ElementPriority(10)]
             public ViewAngle ViewingAngle { get { return this.angle; } set { if (!this.angle.Equals(value)) { this.angle = value; OnElementChanged(); } } }
             [ElementPriority(11)]
-            public bool FlipDirections { get { return this.flip; } set { if (!this.flip.Equals(value)) { this.flip = value; OnElementChanged(); } } }
+            public bool FlipHorizontal { get { return this.flip; } set { if (!this.flip.Equals(value)) { this.flip = value; OnElementChanged(); } } }
             [ElementPriority(12)]
             public float[] Unknown7 { get { return this.unknown7; } set { if (!this.unknown7.Equals(value)) { this.unknown7 = value; OnElementChanged(); } } }
             [ElementPriority(13)]
-            public ulong[] SimModifierReference { get { return this.simModifierReference; } set { if (!this.simModifierReference.Equals(value)) { this.simModifierReference = value; OnElementChanged(); } } }
+            public ulong SimModifierLeft { get { return this.simModifierLeft; } set { if (!this.simModifierLeft.Equals(value)) { this.simModifierLeft = value; OnElementChanged(); } } }
+            [ElementPriority(14)]
+            public ulong SimModifierRight { get { return this.simModifierRight; } set { if (!this.simModifierRight.Equals(value)) { this.simModifierRight = value; OnElementChanged(); } } }
+            [ElementPriority(15)]
+            public ulong SimModifierUp { get { return this.simModifierUp; } set { if (!this.simModifierUp.Equals(value)) { this.simModifierUp = value; OnElementChanged(); } } }
+            [ElementPriority(16)]
+            public ulong SimModifierDown { get { return this.simModifierDown; } set { if (!this.simModifierDown.Equals(value)) { this.simModifierDown = value; OnElementChanged(); } } }
 
             public string Value { get { return ValueBuilder; } }
         }
@@ -251,13 +272,13 @@ namespace CASPartResource
         public enum ViewAngle : byte
         {
             None = 0,
-            FrontView = 1,
-            FrontUnknown1 = 1 << 1,
-            FrontUnknown2 = 1 << 2,
-            SideUnknown = 1 << 3,
-            SideView = 1 << 4,
-            BackView = 1 << 5,
-            BackUnknown = 1 << 6
+            Front = 1<<0,
+            Threequarter_right = 1<<1,
+            Threequarter_left = 1<<2,
+            Profile_right = 1<<3,
+            Profile_left = 1<<4,
+            Back = 1<<5,
+            Unknown = 1 << 6
         }
 
         [Flags]
